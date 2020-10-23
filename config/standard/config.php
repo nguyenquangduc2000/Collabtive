38040da2a1e3184accb037e4c7ca5f74a51b14aa
Nguyen Quang Duc <nguyenquangduc2000@gmail.com>
2020-10-23 20:50:44 +0700

    just a bug

--- config/standard/config.php
+++ config/standard/config.php
@@ -1,9 +1,9 @@
 <?php
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$db_pass = substr($url["path"], 1);
+$db_name = substr($url["path"], 1);
 $db_host = $url["host"];
 $db_user = $url["user"];
 $db_pass = $url["pass"];
 
-$conn = new mysqli($db_host, $db_user, $db_pass, $db_pass);
+$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
 ?>
